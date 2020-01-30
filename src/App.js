e3be909e1835086d179d4c1165f76358fb57ebe1
unknown <yonoraphael@gmail.com>
2020-01-30 22:13:41 +0700

    30 jan 20

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Hero from './Component/Hero';
-import Navibar from './Component/Navibar';
-import About from './Component/About';
-import Project from './Component/Project';
-import Contact from './Component/Contact';
-import Footer from './Component/Footer';
+const Navibar = React.lazy(() => import('./Component/Navibar'));
+const About = React.lazy(() => import('./Component/About'));
+const Project = React.lazy(() => import('./Component/Project'));
+const Contact = React.lazy(() => import('./Component/Contact'));
+const Footer = React.lazy(() => import('./Component/Footer'));
 
 function App() {
   return (
     <div className="App" data-spy="scroll" data-target="#navibar" data-offset="50">
       <Hero/>
+      <React.Suspense fallback={<div>Loading...</div>} >
       <Navibar/>
       <About/>
       <Project/>
       <Contact/>
       <Footer/>
+      </React.Suspense>
     </div>
   );
 }
